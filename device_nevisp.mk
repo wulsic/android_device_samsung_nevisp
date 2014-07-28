cdc65a55d7676a233d1339ac2f61afd3e6dc8c16
Winston <wulsic@hotmail.com>
2014-07-28 09:29:39 +0200

    Update device_nevisp.mk

--- device_nevisp.mk
+++ device_nevisp.mk
@@ -1,11 +1,33 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#
+# Copyright (C) 2013 The Android Open Source Project
+# Copyright (C) 2013 Óliver García Albertos (oliverarafo@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/languages_full.mk)
+
+# Use the Dalvik VM specific for devices with 512 MB of RAM
+$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
+
+# Inherit the proprietary vendors blobs for Samsung Galaxy fame.
+$(call inherit-product-if-exists, vendor/samsung/nevisp/nevisp-vendor.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/samsung/nevisp/nevisp-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/samsung/nevisp/overlay
 
 # LDPI assets
 PRODUCT_AAPT_CONFIG := normal ldpi mdpi nodpi
