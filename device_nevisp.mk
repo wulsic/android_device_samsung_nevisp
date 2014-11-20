6988f1e79e14f01f824e936cce7ec1a28b217f1f
Wulsic <wulsic@hotmail.com>
2014-11-20 17:27:54 +0100

    Use Google's centralized media_codecs parts
    Change-Id: I4c7a03e58b2499e3040a92a8399b7370d9085f67

--- device_nevisp.mk
+++ device_nevisp.mk
@@ -25,7 +25,10 @@ PRODUCT_COPY_FILES += \
 	device/samsung/nevisp/ramdisk/fstab.rhea_ss_nevisp:root/fstab.rhea_ss_nevisp 
 
 PRODUCT_COPY_FILES += \
-device/samsung/nevisp/media_codecs.xml:system/etc/media_codecs.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+        device/samsung/nevisp/media_codecs.xml:system/etc/media_codecs.xml \
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
