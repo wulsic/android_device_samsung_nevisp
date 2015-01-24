5e5770050544ab77ec4997fbf0f6778dfacc48bb
Wulsic <wulsic@hotmail.com>
2015-01-24 20:59:03 +0100

    No if exists, it just must
    Change-Id: Ieba08cad71fbcd04b15ce044dffaeb305277bf53

--- device_nevisp.mk
+++ device_nevisp.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/nevisp/nevisp-vendor.mk)
+$(call inherit-product, vendor/samsung/nevisp/nevisp-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/nevisp/overlay
 
