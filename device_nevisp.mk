7cec56e8f5c6c756c59a225f37887a09653fbebd
Faiz Authar <faizauthar@gmail.com>
2015-11-10 20:19:10 +0700

    made it clean
    Change-Id: I587113e439faae67d7846556622dd654cca7915b

--- device_nevisp.mk
+++ device_nevisp.mk
@@ -40,13 +40,13 @@ PRODUCT_COPY_FILES += \
 
 # Media
  PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml
+     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
  
  PRODUCT_COPY_FILES += \
      frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
      frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
      frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-     device/samsung/nevisp/media_profiles.xml:system/etc/media_profiles.xml
+     device/samsung/nevisp/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Multisim-toggle app © Shubhang Rathore
 PRODUCT_COPY_FILES += \
