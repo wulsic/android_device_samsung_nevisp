4cd93ac18a0c23ebe1b492e71384af421e3338fc
karthick111 <lensflare21@gmail.com>
2016-09-23 21:27:16 +0530

    nevisp: removed partitions from common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,12 +2,19 @@ include device/samsung/rhea-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/nevisp
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/nevisp/bluetooth
+
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_nevisp_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=456M androidboot.console=ttyS0 gpt v3d_mem=67108864 pmem=24M@0x9E800000
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/nevisp/bluetooth
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 939524096
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2236070912
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Recovery
 BOARD_MDPI_RECOVERY := true
